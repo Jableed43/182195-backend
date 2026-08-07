9921358f798d6b4e8a702c5e24bab361ad177d84
Jableed43 <javiernehuenlopez@gmail.com>
2026-08-06 22:02:28 -0300

    feat: format SQL queries for better readability in ejercicios-escuela.sql

--- 6/ejercicios-escuela.sql
+++ 6/ejercicios-escuela.sql
@@ -7,37 +7,37 @@ USE escuela;
 -- G1. Mostrar todos los datos de todos los docentes.
 -- (Resultado esperado: 10 filas)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM escuela.docente;
 
 -- G2. Mostrar únicamente el nombre y el email de todos los estudiantes.
 -- (Resultado esperado: 30 filas, 2 columnas)
 -- Escribe tu consulta aquí:
-
+SELECT nombre, email FROM escuela.estudiante;
 
 -- G3. Mostrar todas las materias dictadas por el docente cuyo ID es 3.
 -- (Resultado esperado: 7 filas)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM materia WHERE docente_id = 3;
 
 -- G4. Mostrar todos los datos de los estudiantes cuyo apellido sea 'Lopez'.
 -- (Resultado esperado: 2 filas)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM escuela.estudiante WHERE apellido = "Lopez"
 
 -- G5. Mostrar las inscripciones que aún no tienen una nota asignada (la nota es nula).
 -- (Resultado esperado: 5 filas)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM escuela.inscripcion WHERE nota IS NULL
 
 -- G6. Mostrar todas las inscripciones donde la nota haya sido mayor a 9.
 -- (Resultado esperado: 5 filas)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM escuela.inscripcion WHERE nota  > 9;
 
 -- G7. Mostrar los datos de la materia cuyo código exacto sea 'BD1'.
 -- (Resultado esperado: 1 fila)
 -- Escribe tu consulta aquí:
-
+SELECT * FROM escuela.materia where codigo  = 'BD1'
 
 -- G8. Mostrar los docentes cuya especialidad sea 'Algoritmos'.
 -- (Resultado esperado: 1 fila)
@@ -47,7 +47,7 @@ USE escuela;
 -- G9. Contar la cantidad total de inscripciones registradas en el sistema.
 -- (Resultado esperado: 56)
 -- Escribe tu consulta aquí:
-
+SELECT COUNT(*) FROM escuela.inscripcion;
 
 -- G10. Mostrar todas las inscripciones que pertenecen al estudiante con ID 5.
 -- (Resultado esperado: 5 filas)
