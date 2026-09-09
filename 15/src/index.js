8e03e33590e941cfb7bd482b25cd3baca45fbd50
Jableed43 <javiernehuenlopez@gmail.com>
2026-09-08 22:03:46 -0300

    feat: implement routes for fetching libros and autores with error handling

--- 15/src/index.js
+++ 15/src/index.js
@@ -1,8 +1,36 @@
 import express from 'express'
+import { conectarDB } from "./db.js";
+import Autor from "./models/autor.js";
+import Libro from "./models/libro.js";
+
+await conectarDB()
 
 const app = express()
 // express.json lee las consultas que recibimos en json y las puede utilizar
 app.use(express.json())
+
+// rutas
+app.get("/libros", async (req, res) => {
+    try {
+        const libros = await Libro
+        .find()
+        .populate("autor", "nombre nacionalidad")
+        .sort({ titulo: 1})
+
+    res.status(200).json(libros)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/autores", async (req, res) => {
+    try {
+        const autores = await Autor.find().sort({nombre: 1})
+        res.status(200).json(autores)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 // Caso 404 en caso que la direccion no exista
 app.use((req, res) => {
